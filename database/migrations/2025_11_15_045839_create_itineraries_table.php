e7b4aaf33d22cc6743edc114a1f622ad1a0592c5
Adre <dreenation21@gmail.com>
2025-11-15 12:03:20 +0700

    feat: add migrations for destinations and itineraries tables

---
+++ database/migrations/2025_11_15_045839_create_itineraries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('itineraries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('destination_id')->constrained()->onDelete('cascade');
+            $table->integer('day_number');
+            $table->text('location');
+            $table->text('description');
+            $table->time('schedule_time')->nullable();
+            $table->text('activities')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('itineraries');
+    }
+};
